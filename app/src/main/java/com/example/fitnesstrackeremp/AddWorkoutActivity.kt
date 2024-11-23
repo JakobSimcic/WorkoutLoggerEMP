ee6024818abaed17db86b66ac38e30afd23408b8
JakobSimcic <147619323+JakobSimcic@users.noreply.github.com>
2024-11-23 16:23:56 +0100

    Dodajanje main page, log in addactivity.

---
+++ app/src/main/java/com/example/fitnesstrackeremp/AddWorkoutActivity.kt
@@ -0,0 +1,13 @@
+package com.example.fitnesstrackeremp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddWorkoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addworkout)
+
+    }
+}
--- app/src/main/java/com/example/fitnesstrackeremp/MainActivity.kt
+++ app/src/main/java/com/example/fitnesstrackeremp/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.fitnesstrackeremp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnLog = findViewById<Button>(R.id.btnLog)
+        val btnAddWorkout = findViewById<Button>(R.id.btnAddWorkout)
+
+        btnLog.setOnClickListener {
+            val intent = Intent(this, LogActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnAddWorkout.setOnClickListener {
+            val intent = Intent(this, AddWorkoutActivity::class.java)
+            startActivity(intent)
         }
     }
 }
+
+
