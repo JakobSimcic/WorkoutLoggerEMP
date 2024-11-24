4c4e5e0ecd015d78686d6c0adc0a46408b92fc20
JakobSimcic <jakob.simcic0@gmail.com>
2024-11-24 18:03:50 +0100

    Popravki

--- app/src/main/java/com/example/fitnesstrackeremp/MainActivity.kt
+++ app/src/main/java/com/example/fitnesstrackeremp/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.fitnesstrackeremp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class AddWorkoutActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
