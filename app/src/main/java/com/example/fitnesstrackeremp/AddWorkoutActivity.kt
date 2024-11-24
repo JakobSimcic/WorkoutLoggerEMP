4f93f084e285d12be14f078a62af9fb827575976
JakobSimcic <jakob.simcic0@gmail.com>
2024-11-24 01:47:50 +0100

    Update

--- app/src/main/java/com/example/fitnesstrackeremp/AddWorkoutActivity.kt
+++ app/src/main/java/com/example/fitnesstrackeremp/AddWorkoutActivity.kt
@@ -1,6 +1,9 @@
 package com.example.fitnesstrackeremp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class AddWorkoutActivity : AppCompatActivity() {
@@ -9,5 +12,31 @@ class AddWorkoutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_addworkout)
 
+        val etWorkoutName = findViewById<EditText>(R.id.etWorkoutName)
+        val etWorkoutDate = findViewById<EditText>(R.id.etWorkoutDate)
+        val etWorkoutType = findViewById<EditText>(R.id.etWorkoutType)
+
+        val etWorkoutDetails = findViewById<EditText>(R.id.etWorkoutDetails)
+
+        val btnSaveWorkout = findViewById<Button>(R.id.btnSaveWorkout)
+
+        btnSaveWorkout.setOnClickListener {
+            val workoutName = etWorkoutName.text.toString()
+            val workoutDate = etWorkoutDate.text.toString()
+            val workoutType = etWorkoutType.text.toString()
+            val workoutDetails = etWorkoutDetails.text.toString()
+
+            if (workoutName.isEmpty() || workoutDate.isEmpty() || workoutType.isEmpty() || workoutDetails.isEmpty()) {
+                Toast.makeText(this, "Prosimo, izpolnite vsa polja.", Toast.LENGTH_SHORT).show()
+            } else {
+
+                Toast.makeText(
+                    this,
+                    "Shranjeno: \nIme: $workoutName\nDatum: $workoutDate\nVrsta: $workoutType\nPodrobnosti: $workoutDetails",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
     }
 }
+
--- app/src/main/java/com/example/fitnesstrackeremp/MainActivity.kt
+++ app/src/main/java/com/example/fitnesstrackeremp/MainActivity.kt
@@ -1,29 +1,15 @@
 package com.example.fitnesstrackeremp
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class AddWorkoutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val btnLog = findViewById<Button>(R.id.btnLog)
-        val btnAddWorkout = findViewById<Button>(R.id.btnAddWorkout)
-
-        btnLog.setOnClickListener {
-            val intent = Intent(this, LogActivity::class.java)
-            startActivity(intent)
-        }
-
-        btnAddWorkout.setOnClickListener {
-            val intent = Intent(this, AddWorkoutActivity::class.java)
-            startActivity(intent)
-        }
+        setContentView(R.layout.activity_addworkout)
     }
 }
+
 
 
